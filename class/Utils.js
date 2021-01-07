cf6c253344cbfffeb32263290b1675d02cb67c39
Maciel Sousa <fmacielsousa@gmail.com>
2021-01-07 16:20:07 -0300

    Editando Objeto

--- class/Utils.js
+++ class/Utils.js
@@ -1,7 +1,7 @@
 class Utils{
 
     static dateFormat(date){
-        
+    
         //Colocando o zero a esquerda na data.
         let dia = (((date.getDate()) < 10) ? '0'+date.getDate() : date.getDate());
         let mes = ((date.getMonth()+1) < 10) ? ''+date.getMonth()+1 : date.getMonth()+1;
--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-let userControllers = new UserControllers("form-user-create", "table-users");
+let userControllers = new UserControllers("form-user-create", "form-user-update", "table-users");
